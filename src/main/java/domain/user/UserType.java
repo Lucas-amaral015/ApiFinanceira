bbd5554e78fed26d9e9f91ad643c96b06c8f66bf
Lucas Amaral <Lucasam@outlook.com>
2025-07-18 09:47:57 -0300

    doc: anotações feitas em `TransactionRepository` e `UserType`.

--- src/main/java/domain/user/UserType.java
+++ src/main/java/domain/user/UserType.java
@@ -1,7 +1,13 @@
 package domain.user;
 
-public enum UserType {// AQUI É UM TIPO ESPECIAL DE CLASSE USADA PARA REPRESENTAR CONJUNTO FIXO
+public enum UserType {
+    // AQUI É UM TIPO ESPECIAL DE CLASSE USADA PARA REPRESENTAR CONJUNTO FIXO
     // DE CONSTANTES.
+    //enum é para padronizar códigos,para coisas definidas que não vai mudar.
+    //exemplo: usuario e os logistas nesse caso.
+    //posso declarar variáveis dentro do enum,eliminando tambem codigos em excesso.
+
+
     COMMOM, //PESSOAS COMUNS
 
     MERCHANT // SÃO OS LOGISTAS
